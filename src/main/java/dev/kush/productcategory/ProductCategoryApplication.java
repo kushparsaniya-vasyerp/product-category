39ed4eb19c1864a225986d3f1d3e2f0140ada99f
kushparsaniya-vasyerp <kushparsaniya@vasyerp.com>
2024-02-09 13:21:02 +0530

    project initialized

---
+++ src/main/java/dev/kush/productcategory/ProductCategoryApplication.java
@@ -0,0 +1,13 @@
+package dev.kush.productcategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCategoryApplication.class, args);
+	}
+
+}
